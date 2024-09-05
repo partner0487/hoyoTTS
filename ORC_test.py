5e8179c747ae4c393fc2ecd25c5d5780e7bf1edb
Partner0487 <partner0487@gmail.com>
2024-09-05 20:39:52 +0800

    更新TTS

--- ORC_test.py
+++
@@ -1,9 +0,0 @@
-# Simple script to perform OCR on the current screen contents.
-
-import screen_ocr
-
-ocr_reader = screen_ocr.Reader.create_quality_reader()
-# To read a cropped region, add bounding_box=(left, top, right, bottom).
-bounding_box=(0, 910, 1920, 1000)
-results = ocr_reader.read_screen(bounding_box)
-print(results.as_string().replace(" ", ""))
--- main.py
+++ main.py
@@ -1,35 +1,67 @@
-import sys
-import parrots
-from parrots import TextToSpeech
-from playsound import playsound 
-import torch
-from loguru import logger
+# Simple script to perform OCR on the current screen contents.
+
 import screen_ocr
+import requests
+import json
+from playsound import playsound 
+import urllib.request
+from opencc import OpenCC
+import os
+from dotenv import load_dotenv
 
-sys.path.append('..')
-parrots_path = parrots.__path__[0]
-sys.path.append(parrots_path)
+load_dotenv()
+access_token = os.getenv("access_token")
 
+cc = OpenCC('t2s')
 ocr_reader = screen_ocr.Reader.create_quality_reader()
 # To read a cropped region, add bounding_box=(left, top, right, bottom).
-bounding_box=(0, 910, 1920, 1000)
-results = ocr_reader.read_screen(bounding_box).as_string().replace(" ", "")
-print(results)
+bounding_box=(800, 800, 1020, 890)
+results = ocr_reader.read_screen(bounding_box)
+speaker = results.as_string().replace(" ", "").replace("\n", "")
+if speaker == "青雀":
+    speaker += "【星穹铁道 】"
+else:
+    speaker += "【原神】"
+speaker = cc.convert(speaker)
+print(speaker)
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-logger.info(f"device: {device}")
-half = True if device == "cuda" else False
+bounding_box=(0, 800, 1920, 980)
+results = ocr_reader.read_screen(bounding_box)
+text = results.as_string().replace(" ", "").replace("\n", "")
+text = cc.convert(text)
+print(text)
 
-m = TextToSpeech(
-    speaker_model_path="shibing624/parrots-gpt-sovits-speaker-maimai",
-    speaker_name="MaiMai",
-    device=device,
-    half=half
-)
+url = 'https://infer.acgnai.top/infer/gen'
+headers = {
+    "content-type": "application/json"
+}
+payload = {
+        "access_token": access_token,
+        "type": "tts",
+        "brand": "gpt-sovits",
+        "name": "anime",
+        "method": "api",
+        "prarm": {
+            "speaker": speaker,
+            "emotion": "开心_happy",
+            "text": text,
+            "text_language":"中文",
+            "text_split_method": "按标点符号切",
+            "fragment_interval": 0.3,
+            "batch_size": 10,
+            "batch_threshold": 0.75,
+            "parallel_infer": True,
+            "split_bucket": True,
+            "top_k": 10,
+            "top_p": 1.0,
+            "temperature": 1.0,
+            "speed_factor": 1.0
+    }
+}
 
-m.predict(
-    text=results,
-    text_language="auto",
-    output_path="main.wav"
-)
+res_json = requests.post(url, json.dumps(payload))
+responseData = res_json.json()
+print(f"message: {responseData['message']}")
+print(f"audio: {responseData['audio']}")
+urllib.request.urlretrieve(responseData['audio'], "main.wav")
 playsound("main.wav", block=True)
